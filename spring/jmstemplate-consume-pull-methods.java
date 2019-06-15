051c44f4834f21fa029b796eeea09beb44f812a1
Tom Fitton <thomas.fitton.17@gmail.com>
2019-06-15 09:39:30 +0100

    Added spring/jmstemplate-custom-pull-methods.java

---
+++ spring/jmstemplate-consume-pull-methods.java
@@ -0,0 +1,7 @@
+Message receive() throws JmsException;
+Message receive(Destination destination) throws JmsException;
+Message receive(String destinationName) throws JmsException;
+
+Object receiveAndConvert() throws JmsException;
+Object receiveAndConvert(Destination destination) throws JmsException;
+Object receiveAndConvert(String destinationName) throws JmsException;
